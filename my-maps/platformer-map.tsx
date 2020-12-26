e05b89ef67f56a2413dbf49722f520411f362428
KoderKlub <atiltoth@gmail.com>
2020-12-26 17:58:18 +0100

    Tiled palya es tileset kep

---
+++ my-maps/platformer-map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.10.30" name="platformerPack_industrial_tilesheet" tilewidth="70" tileheight="70" tilecount="112" columns="14">
+ <image source="platformerPack_industrial_tilesheet.png" width="980" height="560"/>
+</tileset>
